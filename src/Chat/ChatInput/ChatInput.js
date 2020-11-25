195ef191d5a27a6572198ea2575592cbf04a5bc9
dai-yu <1435480344@qq.com>
2020-11-25 16:29:22 +0800

    finish homework

--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  valueChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  inputMessage = () => {
+    const { value } = this.state;
+    if (value !== '') {
+      this.props.onMessageChange(value);
+      this.setState({
+        value: '',
+      });
+    }
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input type="text" onChange={this.valueChange} value={this.state.value} />
+        <button type="button" onClick={this.inputMessage}>
+          Send
+        </button>
       </footer>
     );
   }
